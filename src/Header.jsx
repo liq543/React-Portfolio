9aac2be004521c49f2704c1c9a21bebcf249e1b3
Griffin Gore <griffingore@Griffins-MBP.attlocal.net>
2023-10-13 19:32:42 -0700

    blinking cursor added to the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,30 +1,40 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Home.css';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
+    const [activeRoute, setActiveRoute] = useState('');
+
+    useEffect(() => {
+        setActiveRoute(location.pathname);
+    }, [location]);
 
     const toggleMenu = () => {
-        setMenuOpen(prevState => {
-            return !prevState;
-        });
+        setMenuOpen(prevState => !prevState);
     };
-    
+
+    const renderCursor = (route) => {
+        if (route === activeRoute) {
+            return <span className="text-green-500 blinking mr-2">></span>;
+        }
+    };
 
     return (
-    <header className="bg-black border-white border-2 p-4 flex justify-between items-center z-50 fixed crt">
+        <header className="bg-black border-white border-2 p-4 flex justify-between items-center z-50 fixed crt">
             <h1 className="text-green-500 text-xl font-mono">Griffin Gore's Portfolio</h1>
 
             <nav className="hidden lg:block">
                 <ul className="flex gap-4">
-                    <li><Link to="/home" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Home</Link></li>
-                    <li><Link to="/projects" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Projects</Link></li>
-                    <li><Link to="/resume" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Resume</Link></li>
-                    <li><Link to="/about" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">About Me</Link></li>
-                    <li><Link to="/contact" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Contact</Link></li>
+                    <li>{renderCursor("/home")}<Link to="/home" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Home</Link></li>
+                    <li>{renderCursor("/projects")}<Link to="/projects" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Projects</Link></li>
+                    <li>{renderCursor("/resume")}<Link to="/resume" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Resume</Link></li>
+                    <li>{renderCursor("/about")}<Link to="/about" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">About Me</Link></li>
+                    <li>{renderCursor("/contact")}<Link to="/contact" className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono">Contact</Link></li>
                 </ul>
             </nav>
-            
+
             <button onClick={toggleMenu} className="lg:hidden text-green-500 font-mono z-60">
                 ☰
             </button>
@@ -36,7 +46,7 @@ const Header = () => {
                         <li><Link to="/projects" onClick={toggleMenu} className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono block z-10">Projects</Link></li>
                         <li><Link to="/resume" onClick={toggleMenu} className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono block z-10">Resume</Link></li>
                         <li><Link to="/about" onClick={toggleMenu} className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono block z-10">About Me</Link></li>
-                        <li><Link to="/contact" onClick={toggleMenu} className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono block z-0">Contact</Link></li>
+                        <li><Link to="/contact" onClick={toggleMenu} className="text-green-500 border-white border-2 px-4 py-2 hover:bg-green-500 hover:text-black font-mono block z-10">Contact</Link></li>
                     </ul>
                 </div>
             )}
