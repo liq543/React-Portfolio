d4f7e17d5094f5e31b1aecdd57ed5059002bba77
Griffin Gore <griffingore@Griffins-MBP.attlocal.net>
2023-10-18 18:19:51 -0700

    fixed responsiveness

--- src/ContactScreen.jsx
+++ src/ContactScreen.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const ContactScreen = () => {
   return (
-    <div className="crt flex flex-col items-center justify-center h-screen">
-      <h2 className="text-white font-mono text-6xl mb-10">Let's Connect</h2>
+    <div className="crt flex flex-col items-center justify-center min-h-screen px-4">
+      <h2 className="text-white font-mono text-4xl md:text-6xl mb-10 text-center">Let's Connect</h2>
       
-      <div className="flex space-x-8 text-6xl md:text-8xl">
+      <div className="flex space-x-4 md:space-x-8 text-4xl md:text-6xl lg:text-8xl">
         <a href="https://github.com/liq543" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github text-green-500 hover:text-white"></i>
         </a>
@@ -17,7 +17,7 @@ const ContactScreen = () => {
         </a>
       </div>
 
-      <p className="text-white text-2xl text-center mt-10">
+      <p className="text-white text-xl md:text-2xl text-center mt-10">
         Feel free to connect with me on social media or send me an email. I'm always open to making new connections and exploring opportunities.
       </p>
     </div>
